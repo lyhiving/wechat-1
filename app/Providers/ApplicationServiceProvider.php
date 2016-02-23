25d666041b7e0b9b248b90037b08ffbd9809c9c5
wh469012917 <wenghang1228@gmail.com>
2016-02-23 12:09:33 +0800

    初始化

---
+++ app/Providers/ApplicationServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use EasyWeChat\Foundation\Application;
+
+class ApplicationServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('EasyWeChat\Foundation\Application', function($app)
+        {
+            return new Application([
+                'debug'   => true,
+                'app_id'  => env('APP_ID'),         // AppID
+                'secret'  => env('APP_SECRET'),     // AppSecret
+                'token'   => env('TOKEN'),
+                'log' => [
+                    'level' => 'debug',
+                    'file'  => '/tmp/wechat.log',
+                ]
+            ]);
+        });
+    }
+}
---
+++ resources/views/admin/wechat/user.blade.php
@@ -0,0 +1,49 @@
+@extends('admin.layout')
+@section('content')
+<div id="page-wrapper">
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">粉丝管理</h1>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    全部粉丝
+                </div>
+                <!-- /.panel-heading -->
+                <div class="panel-body">
+                    <div class="dataTable_wrapper">
+                        <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                            <thead>
+                                <tr>
+                                    <th>Rendering engine</th>
+                                    <th>Browser</th>
+                                    <th>Platform(s)</th>
+                                    <th>Engine version</th>
+                                    <th>CSS grade</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="gradeU">
+                                    <td>Other browsers</td>
+                                    <td>All others</td>
+                                    <td>-</td>
+                                    <td class="center">-</td>
+                                    <td class="center">U</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <!-- /.panel-body -->
+            </div>
+            <!-- /.panel -->
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>                                    
+</div>
+@stop
